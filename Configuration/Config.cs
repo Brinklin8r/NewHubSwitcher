31425fa7494195dcfa8c711addf388b2c24e3c12
Chris Brinkley <brinklin8r@outlook.com>
2017-09-01 07:33:29 -0700

    Error Checking missing config values

--- Configuration/Config.cs
+++ Configuration/Config.cs
@@ -8,41 +8,62 @@ using Security;
 
 namespace Configurations {
     public class Config {
-        //<add key = "UIN" value="~5E6B644F777773582A514B2F716C505F5970582F712D7B412A2F742D782F6A622A77616F5B6C56796D65652D5F65656E6E7474656572722D5F79796F6F757572722D5F555549494E4E2D5F6E6E75756D6D626265657272"/>
-		//<add key = "ManagerURL" value="~5C6C5A636C2F59542A622A502A4E404E2A5252757572726C6C7C7C5E5E5B5B5C5C78782D712D6F2D6C5C5C78782D7645455D5D7B7B2D712D6B7D7D2D637C7C50506C6C65656161737365652D5F65656E6E7474656572722D5F7474686865652D5F7373656572727676656572722D5F6E6E61616D6D6565"/>
-		//<add key = "SecondaryManagerURL" value="~5D4C4961707670572A562A57475943502A562A594B625056444F2A5A42572A5D2A45455A4A7575756F6C6C7C7C5E5E5B5B5C5C78782D712D6F2D6C5C5C78782D7645455D5D7B7B2D712D6B7D7D2D637C7C50506C6C65656161737365652D5F65656E6E7474656572722D5F7474686865652D5F7373656563636F6F6E6E64646161727279792D5F7373656572727676656572722D5F6E6E61616D6D6565"/>
-
         private string _UIN = "";
         private string _ManagerURL = "";
         private string _SecondaryManagerURL = "";
+        private string _HubSwitcherDescription = "";
 
         private Encrypt _encObj = new Encrypt();
+        private Configuration _config = ConfigurationManager.OpenExeConfiguration(@"C:\Program Files (x86)\Bluepoint Solutions\RDC\IP Admin\IP Admin.exe");
+
 
         public void ReadConfigFile() {
-            Configuration _config =
-                ConfigurationManager.OpenExeConfiguration(@"C:\Program Files (x86)\Bluepoint Solutions\RDC\IP Admin\IP Admin.exe");
-
             AppSettingsSection _settings = (AppSettingsSection)_config.GetSection("appSettings");
             if(_settings != null) {
-                _UIN = _encObj.DecryptValue(_settings.Settings["UIN"].Value);
-                _ManagerURL = _encObj.DecryptValue(_settings.Settings["ManagerURL"].Value);
-                _SecondaryManagerURL = _encObj.DecryptValue(_settings.Settings["SecondaryManagerURL"].Value);
+                _UIN = _VerrifyValue(_settings.Settings["UIN"].Value);
+                _ManagerURL = _VerrifyValue(_settings.Settings["ManagerURL"].Value);
+
+                // Optional Config entries:
+                // If they do not exist in the Config File default to something
+                try {
+                    _SecondaryManagerURL = _VerrifyValue(_settings.Settings["SecondaryManagerURL"].Value);
+                } catch(Exception) {
+                    _SecondaryManagerURL = _ManagerURL;
+                }
+                try {
+                    _HubSwitcherDescription = _VerrifyValue(_settings.Settings["HubSwitcher"].Value);
+                } catch(Exception) {
+                    _HubSwitcherDescription = "Current";
+                }
+
             }
 
         }
 
         public void WriteConfigFile() {
-            Configuration _config =
-                ConfigurationManager.OpenExeConfiguration(@"C:\Program Files (x86)\Bluepoint Solutions\RDC\IP Admin\IP Admin.exe");
-
             AppSettingsSection _settings = (AppSettingsSection)_config.GetSection("appSettings");
             if(_settings != null) {
                 _settings.Settings["UIN"].Value = _UIN;
                 _settings.Settings["ManagerURL"].Value = _ManagerURL;
                 _settings.Settings["SecondaryManagerURL"].Value = _SecondaryManagerURL;
+                _settings.Settings["HubSwitcher"].Value = _HubSwitcherDescription;
             }
 
             _config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        private string _VerrifyValue(string _fValue) {
+            string _fResult = "";
+
+            if(_fValue.StartsWith("~") && _fValue.Length > 1) {
+                _fResult = _encObj.DecryptValue(_fValue);
+
+            }
+
+
+
+         
+            return _fResult;
         }
     }
 }
